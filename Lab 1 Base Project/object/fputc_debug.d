dd2b6193a7110d98534a76cfb267eacf0049312b
unknown <olafor@156TR4160MP-13.campus.MCGILL.CA>
2015-02-04 19:34:05 -0500

    Bug fixed!
    Signed-off-by: unknown <olafor@156TR4160MP-13.campus.MCGILL.CA>

--- Lab 1 Base Project/object/fputc_debug.d
+++ Lab 1 Base Project/object/fputc_debug.d
@@ -1,8 +1,8 @@
 .\object\fputc_debug.o: source\fputc_debug.c
-.\object\fputc_debug.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\object\fputc_debug.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdio.h
 .\object\fputc_debug.o: source\stm32f4xx.h
 .\object\fputc_debug.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.1\CMSIS\Include\core_cm4.h
-.\object\fputc_debug.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\object\fputc_debug.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
 .\object\fputc_debug.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.1\CMSIS\Include\core_cmInstr.h
 .\object\fputc_debug.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.1\CMSIS\Include\core_cmFunc.h
 .\object\fputc_debug.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.1\CMSIS\Include\core_cm4_simd.h
