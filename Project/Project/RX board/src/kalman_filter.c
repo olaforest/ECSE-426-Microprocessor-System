9a5e6d332e6e7338c21e03353ea8cef4d512a8f0
Genevieve <genevieve.nantel@mail.mcgill.ca>
2015-04-13 10:42:30 -0400

    commented

--- Project/Project/RX board/src/kalman_filter.c
+++ Project/Project/RX board/src/kalman_filter.c
@@ -1,14 +1,16 @@
 /*
-ECSE 426 - Lab 4
+ECSE 426 - Wireless 3D printing machine project
 Maxim Goukhshtein (ID: 260429739)
 Olivier Laforest  (ID: 260469066)
-Group #3
-Date:	March 23, 2015
+Nuri Ege Kozan    (ID: 260359680)
+Genevieve Nantel  (ID: 260481768)
+Group #4
+Date:	April 14th, 2015
 */ 
 
 #include "kalman_filter.h"
 
-// Kalman filter to filter an input. Based on the Kalman filter that was used in Labs 1 and 2.
+// Kalman filter to filter an input. Based on the lab 1 description and used in all the subsequents labs. 
 float kalmanFilter(float input, KalmanState* kstate){
 	float filtered;
 	kstate->p = kstate->p + kstate->q;
