4880050adff84e0ad4a4f6069a5c6c04e920c06a
Genevieve <genevieve.nantel@mail.mcgill.ca>
2015-04-13 13:56:39 -0400

    commented

--- Project/Project/RX board/src/wireless_rx.h
+++ Project/Project/RX board/src/wireless_rx.h
@@ -1,3 +1,12 @@
+/*
+ECSE 426 - Wireless 3D printing machine project
+Maxim Goukhshtein (ID: 260429739)
+Olivier Laforest  (ID: 260469066)
+Nuri Ege Kozan    (ID: 260359680)
+Genevieve Nantel  (ID: 260481768)
+Group #4
+Date:	April 14th, 2015
+*/ 
 #ifndef __WIRELESS_RX__
 #define __WIRELESS_RX__
 
@@ -9,6 +18,7 @@
 #define TIM3_DESIRED_RATE 20
 #define TIM3_PERIOD (TIM3_COUNTER_CLK / TIM3_DESIRED_RATE) - 1
 
+/* Exported functions --------------------------------------------------------*/
 void wireless_init(void);
 void transmit(void);
 void receive(void);
