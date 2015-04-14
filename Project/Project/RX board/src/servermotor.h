1dc009ae3bb8b63045eb3eeec5de39c2c9740489
Genevieve <genevieve.nantel@mail.mcgill.ca>
2015-04-14 08:05:15 -0400

    commented

--- Project/Project/RX board/src/servermotor.h
+++ Project/Project/RX board/src/servermotor.h
@@ -1,8 +1,17 @@
+/*
+ECSE 426 - Wireless 3D printing machine project
+Maxim Goukhshtein (ID: 260429739)
+Olivier Laforest  (ID: 260469066)
+Nuri Ege Kozan    (ID: 260359680)
+Genevieve Nantel  (ID: 260481768)
+Group #4
+Date:	April 14th, 2015
+*/ 
 #ifndef MOTOR_H
 #define MOTOR_H
 
 #include "stm32f4xx.h"
-
+// define lengths that were measured on the mechanical 3d parts
 #define D 33.3375
 #define L1 65.0875
 #define L2 71
@@ -10,7 +19,7 @@
 #define PI 3.14159265358979323846
 #define DELAY    1
 #define NO_DELAY 0
-
+//external functions
 void motor_gpio_init(void);
 void timer_init(void);
 void pwm_angle_init(void);
